0270b87405b9c61817d9061fcfe06720d555247d
17726441075@189.cn <17726441075@189.cn>
2026-08-20 23:47:17 +0800

    流式

---
+++ springai_alibaba/src/main/java/com/example/controller/ChatController.java
@@ -0,0 +1,30 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
+
+import jakarta.annotation.Resource;
+import reactor.core.publisher.Flux;
+
+
+@RestController
+@RequestMapping("/ai")
+public class ChatController {
+    
+    @Resource 
+    private DashScopeChatModel dashScopeModel ;
+    
+    @RequestMapping(value = "/chat/{question}")
+    public Object chat(@PathVariable String question){
+        return dashScopeModel.call(question) ;
+    }
+
+    @RequestMapping(value = "/streamchat/{question}",produces = "text/stream;charset=utf-8")
+    public Flux<String> streamchat(@PathVariable String question){
+        return dashScopeModel.stream(question) ;
+    }
+
+}
