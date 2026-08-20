6f0e38b6764c60934242138c1a7ea3e84b3fb028
17726441075@189.cn <17726441075@189.cn>
2026-08-20 23:30:57 +0800

    SpringAialibaba

---
+++ springai_alibaba/src/test/java/com/example/ChatTest.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
+
+import jakarta.annotation.Resource;
+
+@SpringBootTest(classes = Main.class)
+public class ChatTest {
+    
+
+    @Resource
+    private DashScopeChatModel dashScopeChatModel ;
+
+    @Test
+    public void test1(){
+        System.out.println("sssssssssssssdfdfsdfd");
+        String res = dashScopeChatModel.call("你好") ;
+        System.out.println(res);        
+
+    }
+
+
+}
