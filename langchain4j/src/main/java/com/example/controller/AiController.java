f6f38eb7b597398771aa6a8f66c21f9ae6ae7af6
17726441075@189.cn <17726441075@189.cn>
2026-08-20 20:23:38 +0800

    实现流响应

--- langchain4j/src/main/java/com/example/controller/AiController.java
+++ langchain4j/src/main/java/com/example/controller/AiController.java
@@ -5,7 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.community.model.dashscope.QwenStreamingChatModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
 import jakarta.annotation.Resource;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/ai")
@@ -14,10 +18,42 @@ public class AiController {
     @Resource
     private QwenChatModel qwenChatModel ;
 
+    @Resource
+    private QwenStreamingChatModel qwenStreamingChatModel ;
+
     @RequestMapping("/chat/{question}")
     public Object chat(@PathVariable String question){
         System.out.println(question);
         return qwenChatModel.chat(question);
+    }
+
+    @RequestMapping(value =  "/streamchat/{question}",produces = "text/stream;charset=utf-8")
+    public Flux<String> streamchat(@PathVariable String question){
+        System.out.println(question);
+        return Flux.create(tmp->{
+
+            qwenStreamingChatModel.chat(question, new StreamingChatResponseHandler() {
+
+                @Override
+                public void onPartialResponse(String partialResponse){
+                    tmp.next(partialResponse) ;
+                }
+
+                @Override
+                public void onCompleteResponse(ChatResponse arg0) {
+                    tmp.complete(); 
+                }
+
+                @Override
+                public void onError(Throwable arg0) {
+                    throw new UnsupportedOperationException("Unimplemented method 'onError'");
+                }
+                
+            });
+
+
+
+        }) ;
     }
 
 
