ecbc066babca93e6a7fb7dd1f8609015bcf01326
17726441075@189.cn <17726441075@189.cn>
2026-08-20 19:34:00 +0800

    整合springboot

--- langchain4j/src/main/java/com/example/App.java
+++ langchain4j/src/main/java/com/example/App.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,15 @@ public class App
 {
     public static void main( String[] args )
     {
+        ChatModel openAiChatModel = OpenAiChatModel.builder()
+                                                .apiKey("sk-ws-H.EYMDPRL.XhIH.MEQCIDYUCccAnq5u6Df7hi23dNdb5J5UdJXQP3ixkfNWnPO8AiBq0tfSSlf_YRYDojBuczcFb9djr4X8jn3XSZls1EK0Ag")
+                                                .baseUrl("https://ws-unb6ehiw77dt9w5k.cn-beijing.maas.aliyuncs.com/compatible-mode/v1")
+                                                .modelName("qwen3.8-max")
+                                                .build(); ;
+        String res = openAiChatModel.chat("你好,你是谁");
+        System.out.println(res);
+
+
         System.out.println( "Hello World!" );
     }
 }
