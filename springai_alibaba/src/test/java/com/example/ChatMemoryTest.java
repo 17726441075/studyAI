a635f00135083dba10603a50ecc26fc956ebe8f4
17726441075@189.cn <17726441075@189.cn>
2026-08-21 21:03:48 +0800

    准备加入向量化

--- springai_alibaba/src/test/java/com/example/ChatMemoryTest.java
+++ springai_alibaba/src/test/java/com/example/ChatMemoryTest.java
@@ -1,13 +1,10 @@
 package com.example;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.messages.AssistantMessage;
-import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Autowired;
