4248dac94bb7392dd82e54bf32b5ba5ba61717be
17726441075@189.cn <17726441075@189.cn>
2026-08-21 22:03:28 +0800

    更正向量数据库

--- springai_alibaba/src/test/java/com/example/EmbeddingTest.java
+++ springai_alibaba/src/test/java/com/example/EmbeddingTest.java
@@ -1,10 +1,14 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingResponse;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -33,6 +37,23 @@ public class EmbeddingTest {
         log.info("向量维度：" + embedding.length);
         // 输出向量内容（前10维，便于查看）
         log.info("向量前10维：" + Arrays.toString(Arrays.copyOf(embedding, 10)));
+    }
+
+    // 存储
+    @Test
+    public void test01(@Autowired VectorStore milvusVectorStore) {
+        List<String> batchTexts = new ArrayList<>();
+        batchTexts.add("【2025新款】智能保温杯 316不锈钢 温度显示 24小时保温 白色 500ml");
+        batchTexts.add("无线蓝牙耳机 半入耳式 降噪高清通话 续航40小时 适配安卓/苹果");
+        batchTexts.add("机械键盘 青轴 104键 背光有线 电竞游戏专用 全键无冲 黑色");
+        batchTexts.add("家用投影仪 1080P高清 自动对焦 5G双频WiFi 兼容4K 白色 便携款");
+        batchTexts.add("电动牙刷 超声波清洁 5种模式 续航90天 软毛刷头 成人款 蓝色");
+
+        List<Document> documents = batchTexts.stream()
+                                            .map(Document::new)
+                                            .collect(Collectors.toList());
+        // 在这里将我们之前的文档直接调用wriete方法写入到miluvs中
+        milvusVectorStore.write(documents);
     }
 
 }   
